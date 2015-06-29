a933405171c5c5d68833047a9b960ca98f6a317e
ayeowch <ayeowch@gmail.com>
2015-06-29 11:14:07 +1000

    Recreate migrations with Django < 1.8.

--- access_log/migrations/0001_initial.py
+++ access_log/migrations/0001_initial.py
@@ -9,8 +9,8 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('contenttypes', '0001_initial'),
     ]
 
     operations = [
@@ -29,5 +29,6 @@ class Migration(migrations.Migration):
                 'abstract': False,
                 'get_latest_by': 'pk',
             },
+            bases=(models.Model,),
         ),
     ]
