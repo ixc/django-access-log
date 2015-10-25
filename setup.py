8fad79dfd382f36a482ea2232972756045f269e4
Tai Lee <real.human@mrmachine.net>
2015-10-26 00:46:48 +1100

    Updates for distribution on package index.
    * Ignore dist files.
    * Build universal wheels for distribution.
    * Convert `README.md` to reStructuredText to use as long description.
    * Do not import app package.
    * Rename app package.
    * Add required metadata.
    * Include all files as package data.

--- setup.py
+++ setup.py
@@ -1,11 +1,35 @@
-import setuptools
+from __future__ import print_function
 
-from access_log import __version__
+import setuptools
+import sys
+
+version = '0.1.dev0'
+
+# Convert README.md to reStructuredText.
+if {'bdist_wheel', 'sdist'}.intersection(sys.argv):
+    try:
+        import pypandoc
+    except ImportError:
+        print('ERROR: You must install `pypandoc` to convert `README.md` to '
+              'reStructuredText to use as long description.',
+              file=sys.stderr)
+        exit(1)
+    print('Converting `README.md` to reStructuredText to use as long '
+          'description.')
+    long_description = pypandoc.convert('README.md', 'rst')
 
 setuptools.setup(
-    name='access_log',
-    version=__version__,
+    name='django-access-log',
+    version=version,
+    author='Interaction Consortium',
+    author_email='studio@interaction.net.au',
+    url='https://github.com/ixc/django-access-log',
+    description='Track and generate statistics for user access to your Django '
+                'model instances in your project.',
+    long_description=locals().get('long_description', ''),
+    license='MIT',
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
         'coverage',
         'django-dynamic-fixture',
